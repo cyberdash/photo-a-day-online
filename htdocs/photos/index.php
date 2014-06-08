5e1c660a4350359852a1aaccecf67d762427f7fa
Joshua Nelson <jonelson@atlassian.com>
2014-06-08 20:37:19 +1000

    Very basic view photos page

--- htdocs/photos/index.php
+++ htdocs/photos/index.php
@@ -1,1 +1,9 @@
-Nothing to see here. Yet.
+<html>
+	<head>
+		<link rel="stylesheet" href="/css/photos.css" type="text/css" />
+	</head>
+
+	<body>
+		<img src="2.jpeg" class="pad-photo"/>
+	</body>
+</html>
