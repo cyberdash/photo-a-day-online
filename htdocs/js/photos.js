29cfe87231caf521fd9a90517ddcf04c1ffb864b
Joshua Nelson <jonelson@atlassian.com>
2014-06-09 22:00:47 +1000

    Added javascript files

---
+++ htdocs/js/photos.js
@@ -0,0 +1,44 @@
+$(function(){
+
+	var LOADING_CLASS = 'loading';
+
+	function getCenterImageSource() {
+		var centerElement = document.elementFromPoint(window.screen.availWidth / 2, window.screen.availHeight / 2);
+		var centerImageSource = $(centerElement).attr('data-filename');
+		return centerImageSource;
+	}
+
+	function startLoading($el) {
+		$el.addClass(LOADING_CLASS);
+	}
+
+	function stopLoading($el) {
+		$el.removeClass(LOADING_CLASS);
+	}
+
+	function isLoading($el) {
+		return $el.hasClass(LOADING_CLASS);
+	}
+
+	$(window).scroll(function() {
+		var $preview = $('#preview');
+		if (!isLoading($preview)) {
+			startLoading($preview);
+		}
+
+	});
+	$(window).scroll($.debounce(function(){
+		var $preview = $('#preview');
+
+		stopLoading($preview);
+
+		$preview.hide();
+		var newImageSource = getCenterImageSource();
+		$preview.show();
+
+		var $newImage = $('<img/>').attr('src', newImageSource);
+
+		$preview.append($newImage);
+		$preview.find('img').first().remove();
+	}, 500));
+});
