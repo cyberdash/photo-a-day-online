3edd84c3e1ff772bf05f1dc662a8c96f35d79b9b
Joshua Nelson <jonelson@atlassian.com>
2014-08-10 23:16:29 +1000

    Use date sorting rather than alphabetical

--- htdocs/photolist.php
+++ htdocs/photolist.php
@@ -1,7 +1,24 @@
 <?php
 $photo_list = array();
 
-$files = scandir("photos");
+function scandir_date($dir) {
+    $ignored = array('.', '..', '.svn', '.htaccess');
+
+    $files = array();    
+    foreach (scandir($dir) as $file) {
+        if (in_array($file, $ignored)) continue;
+        $files[$file] = filemtime($dir . '/' . $file);
+    }
+
+    arsort($files);
+    $files = array_keys($files);
+
+    return ($files) ? $files : false;
+}
+
+
+
+$files = scandir_date("photos");
 foreach ($files as $file) {
 	if (in_array($file, [".", ".."])) {
 		continue;
