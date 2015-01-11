5d2a9f18b17e1124d553a372a0836757093151c5
Joshua Nelson <jonelson@atlassian.com>
2015-01-11 15:22:42 +1100

    thumbnail is now relative too

--- htdocs/photolist.php
+++ htdocs/photolist.php
@@ -23,7 +23,7 @@ foreach ($files as $file) {
 	if (in_array($file, [".", ".."])) {
 		continue;
 	}
-	$thumbnail = "/thumbnail.php?img=photos/$file&h=100";
+	$thumbnail = "thumbnail.php?img=photos/$file&h=100";
 	$photo_markup = '<img data-filename="photos/' . $file . '" src="' . $thumbnail . '" data-original="' . $thumbnail . '" class="photo-small"/>';
 	array_push($photo_list, $photo_markup);
 }
